4d1b723f3fa1df40c0ef21111bd0fb7f20de4aac
Javier Combarros <Javier@138.4.192.251>
2015-05-15 13:01:51 +0200

    Added DI to TestController

--- src/main/java/es/jab/business/ws/test/TestController.java
+++ src/main/java/es/jab/business/ws/test/TestController.java
@@ -1,4 +1,4 @@
-package es.jab.business.ws;
+package es.jab.business.ws.test;
 
 
 import java.io.IOException;
@@ -6,14 +6,18 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
+	
+	@Autowired
+	private TestMessage testMessage;
      
 	@RequestMapping(value = {"/Test"}) 
     public void prueba(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws IOException {		
-        httpServletResponse.getWriter().println("Hello World!");
+        httpServletResponse.getWriter().println(testMessage.printMessage());
     }
 }
