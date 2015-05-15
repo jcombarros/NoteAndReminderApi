2e7cbe5eed4248b464b58f3fc73f293e76325e06
Javier Combarros <Javier@138.4.192.251>
2015-05-15 12:32:16 +0200

    Spring WS Configured

---
+++ src/main/java/es/jab/business/ws/TestController.java
@@ -0,0 +1,19 @@
+package es.jab.business.ws;
+
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+     
+	@RequestMapping(value = {"/Test"})
+    public void prueba(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws IOException {		
+        httpServletResponse.getWriter().println("Hello World!");
+    }
+}
