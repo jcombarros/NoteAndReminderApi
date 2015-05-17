cbbad33fc68658e6b22dc554e68cfe23e4b46ad2
Javier Combarros <Javier@138.4.192.251>
2015-05-17 19:40:20 +0200

    Json Transformer implemented using Jackson

---
+++ src/main/java/es/jab/utils/json/jackson/JsonTransformerImplJackson.java
@@ -0,0 +1,34 @@
+package es.jab.utils.json.jackson;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.jab.utils.json.JsonTransformer;
+
+public class JsonTransformerImplJackson implements JsonTransformer {
+
+    @Override
+    public String toJson(Object data) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            return objectMapper.writeValueAsString(data);
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public Object fromJson(String json, Class clazz) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            return objectMapper.readValue(json, clazz);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
