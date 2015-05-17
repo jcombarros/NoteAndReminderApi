c1ec486d83a5b758fce5b980db30eb78cf887c73
Javier Combarros <Javier@138.4.192.251>
2015-05-17 22:38:17 +0200

    Modified Jackson Json Date format

--- src/main/java/es/jab/utils/json/jackson/JsonTransformerJackson.java
+++ src/main/java/es/jab/utils/json/jackson/JsonTransformerJackson.java
@@ -1,6 +1,8 @@
 package es.jab.utils.json.jackson;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,12 +10,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import es.jab.utils.json.JsonTransformer;
 
 public class JsonTransformerJackson implements JsonTransformer {
+	
+	private ObjectMapper objectMapper;
+	
+	public JsonTransformerJackson() {
+		this.objectMapper = new ObjectMapper();
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		this.objectMapper.setDateFormat(df);
+	}
 
     @Override
     public String toJson(Object data) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-
             return objectMapper.writeValueAsString(data);
         } catch (JsonProcessingException ex) {
             throw new RuntimeException(ex);
@@ -23,8 +31,6 @@ public class JsonTransformerJackson implements JsonTransformer {
     @Override
     public Object fromJson(String json, Class clazz) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-
             return objectMapper.readValue(json, clazz);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
