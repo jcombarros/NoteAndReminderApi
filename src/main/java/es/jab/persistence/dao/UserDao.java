98b7116b60bcee26b3403f2e1c7b9997d6239770
Javier Combarros <Javier@138.4.192.251>
2015-05-30 16:17:37 +0200

    getUserByEmail() method added in DAO

--- src/main/java/es/jab/persistence/dao/UserDao.java
+++ src/main/java/es/jab/persistence/dao/UserDao.java
@@ -3,5 +3,7 @@ package es.jab.persistence.dao;
 import es.jab.persistence.model.User;
 
 public interface UserDao extends GenericDao<User, Integer> {
+	
+	public User getUserByEmail(String email);
 
 }
--- src/main/java/es/jab/persistence/dao/jpa/UserDaoJpa.java
+++ src/main/java/es/jab/persistence/dao/jpa/UserDaoJpa.java
@@ -1,5 +1,11 @@
 package es.jab.persistence.dao.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import es.jab.persistence.dao.UserDao;
 import es.jab.persistence.model.User;
 
@@ -10,5 +16,26 @@ public class UserDaoJpa extends GenericDaoJpa<User,Integer>implements UserDao {
     public UserDaoJpa() {
         super(User.class);
     }
+
+	@Override
+	public User getUserByEmail(String email) {
+		EntityManager entityManager = GenericDaoJpa.getEntityManagerFactory().createEntityManager();
+		User user;
+		try{
+	        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+	        CriteriaQuery<User> query = builder.createQuery(User.class);
+			Root<User> root =  query.from(User.class);
+			query.select(root);
+			query.where(builder.equal(root.get("email"), email));
+			TypedQuery<User> typedQuery = entityManager.createQuery(query);
+			user = typedQuery.getSingleResult();
+		}
+		catch(Exception e){
+			user = null;
+		}finally{
+			entityManager.close();
+		}
+		return user;
+	}
     
 }
